8e275f3eb4e436ddcfdfdc968f58cfac2bb31752
BetEdwin <93927743+BetEdwin@users.noreply.github.com>
2021-11-09 10:50:16 -0500

    print in python

---
+++ main.py
@@ -0,0 +1,14 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+messaje="hello world enjoy life"
+_float=113.23
+_integer=123
+
+print(messaje)
+print(type(messaje))
+
+print(_float)
+print(type(_float))
+
+print(_integer)
+print(type(_integer))
