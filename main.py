a741a24877f15b80f253dd33fbe654de051007c4
BetEdwin <93927743+BetEdwin@users.noreply.github.com>
2021-11-09 11:02:49 -0500

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+
 # Online Python compiler (interpreter) to run Python online.
 # Write Python 3 code in this online editor and run it.
-messaje="hello world enjoy life"
-_float=113.23
-_integer=123
+messaje1="hello world"
+messaje2="-enjoy life"
+_float1=113.23
+_float2=135.24
+_integer1=123
+_integer2=123
 
-print(messaje)
-print(type(messaje))
+addition=messaje1+messaje2
+addition2=_float2+_float1
+addition3=_integer1+_integer2
 
-print(_float)
-print(type(_float))
 
-print(_integer)
-print(type(_integer))
+print("La addition es: ",addition)
+print("La addition es: ",addition2)
+print("La addition es: ",addition3)
