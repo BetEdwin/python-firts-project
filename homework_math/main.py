ef616e5ec3a1fc3cff51a490afd5b94bbe71e320
BetEdwin <93927743+BetEdwin@users.noreply.github.com>
2021-11-16 11:50:18 -0500

    Add files via upload

---
+++ homework_math/main.py
@@ -0,0 +1,7 @@
+import math
+print("Por favor ingrese el radio del circulo")
+radio=float(input())
+pi=math.pi
+AREA=math.pi*pow(radio,2)
+print ( "El area del circulo es:",AREA,"el radio es",radio)
+
