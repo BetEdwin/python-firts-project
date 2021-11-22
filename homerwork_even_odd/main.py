b7327ad9216157fe774284dbf1cda920ea4754b0
BetEdwin <edwin.betancourt@uptc.edu.co>
2021-11-22 18:38:28 -0500

    homework for know if a number is even or odd

---
+++ homerwork_even_odd/main.py
@@ -0,0 +1,8 @@
+# homework for know if a number is even or odd
+num=int(input("enter a number"))
+# conditional for make desicion
+if num%2==0:
+    print("number is even")
+else:
+    print("number is odd")
+
--- homework_even_odd/main.py
+++
@@ -1,8 +0,0 @@
-#homework for know if number odd or even
-dato=int(input("enter a number"))
-#conditional to make a decision
-if dato % 2 == 0:
-  print("number even")
-else:
-  print("number odd")
-
