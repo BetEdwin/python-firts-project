3c7c961fd84ae5e6aa561c5b70b8a226fd4b5c22
BetEdwin <93927743+BetEdwin@users.noreply.github.com>
2021-11-30 19:59:46 -0500

    BASIC CALCULATOR

--- homework_calculator/main.py
+++ homework_calculator/main.py
@@ -1,27 +1,3 @@
-
-print("basic calculator-----(+)(-)(*)(/)---- ")
-num=float(input("enter a number1"))
-num1=float(input("enter a number2"))
-operation=float(input("what operation do you want to perform\n1- addition\n2- subtraction\n3- -multiplication\n4- division\n--- and write the term that accompanies the math operation\n"))
-#\n is
-if operation == 1:
-    print("result")
-    print(float(num+num1))
-
-elif operation == 2:
-    print("result")
-    print(float(num-num1))
-
-elif operation == 3:
-    print("result")
-    print(float(num * num1))
-
-elif operation !=0:
-    print("result is ")
-    print(float(num/num1))
-else:
-    print("the operation is not possible")
-
 def print_welcome_message():
     print("basic calculator-----(+)(-)(*)(/)---- ")
 
